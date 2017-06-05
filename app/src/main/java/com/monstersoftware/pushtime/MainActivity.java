23ffecae3b8b8ef6dcf8766bc3f59a675bea955c
arizmendi <jr_777_arizmendi@hotmail.com>
2017-06-05 08:56:33 -0500

    Desde elementaryOS

--- app/src/main/java/com/monstersoftware/pushtime/MainActivity.java
+++ app/src/main/java/com/monstersoftware/pushtime/MainActivity.java
@@ -1,5 +1,6 @@
 package com.monstersoftware.pushtime;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -47,6 +48,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null){
                     Log.i("SESION","sesion iniciadacon email:"+ user.getEmail());
+                    goProyectos();
+
+
                 }else{
                     Log.i("SESION","Sesion cerrada");
 
@@ -61,49 +65,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     }
-    private void registrar (String email, String pass){
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    Log.i("SESION","Usuario creado correctamente");
-                }else{
-                    Log.e("SESION",task.getException().getMessage()+"");
-                }
 
-            }
-        });
-
+    public void goProyectos() {
+        Intent intent = new Intent(this,ListadeProyectos.class);
+        startActivity(intent);
     }
-    private void iniciar (String email, String pass){
+
+
+    public void iniciar (String email, String pass){
         FirebaseAuth.getInstance().signInWithEmailAndPassword(email,pass);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
 
 
 
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onClick(View view) {
@@ -114,14 +90,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 iniciar(emailini ,passini);
                 break;
             case R.id.register:
-                String emailreg = editTextEmail.getText().toString();
-                String passreg = editTextPass.getText().toString();
-                registrar(emailreg ,passreg);
+                goRegistrarse();
                 break;
 
 
 
         }
+    }
+
+    private void goRegistrarse() {
+        Intent intent = new Intent(this,activityRegistro.class);
+        startActivity(intent);
     }
 
     @Override
---
+++ app/src/main/java/com/monstersoftware/pushtime/activityRegistro.java
@@ -0,0 +1,89 @@
+package com.monstersoftware.pushtime;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class activityRegistro extends AppCompatActivity {
+    EditText editTextemail1,editTextemail2,editTextpass1,editTextpass2;
+    Button boton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        editTextemail1 =(EditText)findViewById(R.id.editText_email1);
+        editTextemail2 =(EditText)findViewById(R.id.editText_email2);
+        editTextpass1 =(EditText)findViewById(R.id.editTextpass1);
+        editTextpass2 =(EditText)findViewById(R.id.editTextpass2);
+        boton = (Button)findViewById(R.id.registrarse);
+
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email1, email2, pass1, pass2,email = null,pass = null;
+                email1 = editTextemail1.getText().toString();
+                email2 = editTextemail2.getText().toString();
+                pass1 = editTextpass1.getText().toString();
+                pass2 = editTextpass2.getText().toString();
+
+
+                if (email1.contentEquals(email2)){
+                    email = email1;
+
+                }else{
+                    Toast.makeText(activityRegistro.this,"Los correos no son iguales.",Toast.LENGTH_LONG).show();
+
+                }
+                if(pass1.contentEquals(pass2)){
+                    pass = pass1;
+                }else{
+                    Toast.makeText(activityRegistro.this,"Las contraseñas no coinciden.",Toast.LENGTH_LONG).show();
+
+                }
+                registrar(email ,pass);
+                goProyectos();
+
+
+            }
+        });
+
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void goProyectos() {
+        Intent intent = new Intent(this,ListadeProyectos.class);
+        startActivity(intent);
+    }
+
+    private void registrar(String email, String pass) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,pass);
+    }
+
+}
